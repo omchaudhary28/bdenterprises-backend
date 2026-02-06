ffe62f778b681e59f54cd95d376050985adec5e8
omchaudhary28 <omchaudhary2111@gmail.com>
2026-02-07 02:02:07 +0530

    Allow localhost origins for dev CORS

--- api/cors.php
+++ api/cors.php
@@ -1,7 +1,9 @@
 <?php
 $allowed_origins = ['https://bdenterprises.in', 'https://www.bdenterprises.in'];
 $origin = $_SERVER['HTTP_ORIGIN'] ?? '';
-if (in_array($origin, $allowed_origins, true)) {
+$is_local_dev_origin = preg_match('/^https?:\/\/(localhost|127\.0\.0\.1)(:\d+)?$/', $origin) === 1;
+
+if ($origin !== '' && (in_array($origin, $allowed_origins, true) || $is_local_dev_origin)) {
     header('Access-Control-Allow-Origin: ' . $origin);
     header('Vary: Origin');
 }
